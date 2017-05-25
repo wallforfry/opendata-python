1ccfd320190ae140ed6b343b625768e9b897d148
Wallerand Delevacq <wallforfry@gmail.com>
2017-05-25 12:35:21 +0200

    First Release v1.0

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Date : 28/03/2017
 import os, sys
 from ihm import launch_gui
 progname = os.path.basename(sys.argv[0])
-progversion = "0.1"
+progversion = "1.0"
 
 if __name__ == "__main__":
     print("HelloWorld!")
